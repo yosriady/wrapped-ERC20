065778a9a7f7a5cb7e90b0e26c6904a4a3b41b3b
Yos Riady <yos.riady@tenx.tech>
2019-09-30 14:00:07 +0800

    Complete WrappedERC20.test.js

--- test/WrappedERC20.test.js
+++ test/WrappedERC20.test.js
@@ -1,10 +1,13 @@
+const { assertEvent } = require('./helpers');
+
 const WrappedERC20 = artifacts.require("WrappedERC20");
 
 const TOKEN_NAME = 'Wrapped PAY';
 const TOKEN_SYMBOL = 'wPAY';
 const TOKEN_DECIMALS = 18;
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-contract("WrappedERC20", () => { 
+contract("WrappedERC20", ([minter, payee, stranger]) => { 
   before(async () => {
     // Deploys Wrapped ERC20 token
     this.token = await WrappedERC20.new(
@@ -25,10 +28,48 @@ contract("WrappedERC20", () => {
   });
 
   it('should be mintable', async () => {
-    
+    const isMinter = await this.token.isMinter(minter);
+    assert.equal(isMinter, true);
+
+    const balance = await this.token.balanceOf(payee);
+    assert.equal(balance, 0);
+
+    const result = await this.token.mint(payee, 123, { from: minter });
+    assertEvent(result, {
+      event: 'Transfer',
+      args: {
+        from: ZERO_ADDRESS,
+        to: payee,
+        value: 123,
+      },
+    }, 'A Minted event is emitted.');
+
+    const newBalance = await this.token.balanceOf(payee);
+    assert.equal(newBalance, 123);
   });
 
-  it('should be burnable', async () => {
-    
+  it('should be burnable by owner', async () => {
+    const result = await this.token.burn(100, { from: payee });
+
+    assertEvent(result, {
+      event: 'Transfer',
+      args: {
+        from: payee,
+        to: ZERO_ADDRESS,
+        value: 100,
+      },
+    }, 'A Burned event is emitted.');
+
+    const newBalance = await this.token.balanceOf(payee);
+    assert.equal(newBalance, 23);
+  });
+
+  it('should be burnable by allowance', async () => {
+    await this.token.approve(stranger, 23, { from: payee });
+
+    await this.token.burnFrom(payee, 23, { from: stranger });
+
+    const newBalance = await this.token.balanceOf(payee);
+    assert.equal(newBalance, 0);
   });
 });
