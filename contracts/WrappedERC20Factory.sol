8cdee497b2bb4417a92012797d03211dcd39d974
Yos Riady <yos.riady@tenx.tech>
2019-10-04 18:19:53 +0800

    Refactor WrappedERC20Factory

--- contracts/WrappedERC20Factory.sol
+++ contracts/WrappedERC20Factory.sol
@@ -20,13 +20,23 @@ contract WrappedERC20Factory {
 
     // Deploys WrappedERC20 and Exchange contract
     function create(IERC20 _token) public {
+        IERC20MintableBurnable wrappedToken = createWrappedToken(_token);
+        IExchange exchange = createExchange(_token, wrappedToken);
+
+        // Handover ERC20Mintable mint permissions from deployer to Exchange contract
+        MinterRole mintable = MinterRole(address(wrappedToken));
+        mintable.addMinter(address(exchange));
+        mintable.renounceMinter();
+    }
+
+    // Deploy Wrapped Token contract
+    function createWrappedToken(IERC20 _token) internal returns (IERC20MintableBurnable) {
         // Get name, symbol, and decimals directly from _token, via ERC20Detailed
         IERC20Detailed t = IERC20Detailed(address(_token));
         string memory wrappedName = string(abi.encodePacked("Wrapped", " ", t.name()));
         string memory wrappedSymbol = string(abi.encodePacked("w", t.symbol())); // e.g. wPAY
-        uint8 wrappedDecimals = uint8(t.decimals()); // Conversion from old uint256 decimals to new uint8
+        uint8 wrappedDecimals = uint8(t.decimals()); // Conversion from old ERC20 uint256 decimals to new uint8
 
-        // Deploy Wrapped Token contract
         IERC20MintableBurnable wrappedToken = new WrappedERC20(
           wrappedName,
           wrappedSymbol,
@@ -35,17 +45,18 @@ contract WrappedERC20Factory {
         wrappedTokens[address(_token)] = wrappedToken;
         emit WrappedTokenCreated(address(_token), address(wrappedToken));
 
-        // Deploy Token <> Wrapped Token Exchange contract
+        return wrappedToken;
+    }
+
+    // Deploy Token <> Wrapped Token Exchange contract
+    function createExchange(IERC20 _token, IERC20MintableBurnable _wrappedToken) internal returns (IExchange) {
         IExchange exchange = new WrappedERC20Exchange(
           _token,
-          wrappedToken
+          _wrappedToken
         );
         exchanges[address(_token)] = exchange;
         emit ExchangeCreated(address(_token), address(exchange));
 
-        // Handover ERC20Mintable mint permissions from deployer to Exchange contract
-        MinterRole mintable = MinterRole(address(wrappedToken));
-        mintable.addMinter(address(exchange));
-        mintable.renounceMinter();
+        return exchange;
     }
 }
