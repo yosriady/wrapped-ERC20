dc99d1ced8fb0536c23214d5f2cef87b5e7d46b0
Yos Riady <yos.riady@tenx.tech>
2019-10-17 20:06:57 +0800

    Update latest Rinkeby ABIs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
 const WrappedERC20Factory = artifacts.require("WrappedERC20Factory");
 
 module.exports = async (deployer) => {
-  await deployer.deploy(SimpleStorage); // for sanity check
   await deployer.deploy(WrappedERC20Factory);
 };
--- truffle.js
+++ truffle.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ["truffle-security"],
 
   // See <http://truffleframework.com/docs/advanced/configuration> to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"), // ABIs are written here
   networks: {
     development: { // Ganache
       host: '127.0.0.1',
