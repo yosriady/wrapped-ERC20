61bba72652598a1f73b05a032a436fb8e530e963
Yos Riady <yos.riady@tenx.tech>
2019-10-29 00:43:09 +0800

    Fix script

--- scripts/addLiquidity.js
+++ scripts/addLiquidity.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 const argv = require('minimist')(process.argv.slice(), { string: ['network', 'token'] });
+const BN = require('bn.js');
 const Web3 = require('web3');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const contract = require('truffle-contract');
 const { getNodeEndpoint, getNetworkId } = require('./utils');
 const UniswapExchangeABI = require('../artifacts/uniswap/Exchange.json');
-const WrappedERC20 = require('../artifacts/abis/WrappedERC20.json');
+const { abi: WrappedERC20ABI } = require('../artifacts/abis/WrappedERC20.json');
 
 // Start of configurable parameters
 const MAINNET_UNISWAP_EXCHANGE_ADDRESS = '';
 const RINKEBY_UNISWAP_EXCHANGE_ADDRESS = '0x71e5561e12bc4a5dc21536193ec9d7f0c48b4a19';
 const MAINNET_WPAY_ADDRESS = '';
 const RINKEBY_WPAY_ADDRESS = '0xecdba5e120abf076b65cd480476bb2bdda28ad26';
-const INITIAL_LIQUIDITY = 1000000000000000000000; // 200 PAY
+const INITIAL_LIQUIDITY = new BN('200000000000000000000'); // 200 PAY
+const NEW_LIQUIDITY = INITIAL_LIQUIDITY; // TODO: parameterize for CLI
 const NOW = Math.round(Date.now() / 1000);
 // End of configurable parameters
 
@@ -50,7 +52,7 @@ async function main() {
   const accounts = await web3.eth.getAccounts();
   const defaultAccount = accounts[0];
 
-  const WPAY = contract({ abi: WrappedERC20 });
+  const WPAY = contract({ abi: WrappedERC20ABI });
   WPAY.setProvider(provider);
   WPAY.setNetwork(getNetworkId(network));
   WPAY.defaults({
@@ -68,10 +70,9 @@ async function main() {
   const exchangeAddress = getExchangeAddress(network);
   const exchange = await UniswapExchange.at(exchangeAddress);
 
-  console.log('Approving ... wPAY to Uniswap exchange ${exchangeAddress}')
-  await wpay.approve(exchangeAddress, exchangeAddress);
+  console.log(`Approving ${NEW_LIQUIDITY} wPAY to Uniswap exchange ${exchangeAddress}`)
+  await wpay.approve(exchangeAddress, NEW_LIQUIDITY);
   console.log('Approve Completed');
-
 
   console.log(`Adding liquidity to Uniswap exchange ${exchangeAddress}`);
   // https://docs.uniswap.io/frontend-integration/pool#add-liquidity
@@ -82,7 +83,7 @@ async function main() {
   // [17 Oct] ETH is $176.21, PAY is $0.087 -> 1 ETH ~= 2025 PAY, 0.1 ETH ~= 203 PAY
   // TODO: parameterize these variables
   const min_liquidity = 0; // 0 for first time liquidity is added
-  const max_tokens = INITIAL_LIQUIDITY
+  const max_tokens = NEW_LIQUIDITY
   const deadline = NOW + 300; // 5 minutes (300 secs) from now
   const ethAmount = web3.utils.toWei('0.1', 'ether');
   await exchange.addLiquidity(min_liquidity, max_tokens, deadline, { value: ethAmount });
