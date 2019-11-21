535e6fa480bc3065a7af4708700778336714e4c4
Yos Riady <yos.riady@tenx.tech>
2019-11-21 11:22:12 +0800

    Initial commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-import { DrizzleProvider } from "drizzle-react";
-import { LoadingContainer } from "drizzle-react-components";
+import { DrizzleProvider } from "@drizzle/react-plugin";
+import { LoadingContainer } from "@drizzle/react-components";
 
 import "./App.css";
 
 import drizzleOptions from "./drizzleOptions";
-import MyComponent from "./MyComponent";
-import store from './middleware'
+import MyContainer from "./MyContainer";
 
 class App extends Component {
   render() {
     return (
-      <DrizzleProvider store={store} options={drizzleOptions}>
+      <DrizzleProvider options={drizzleOptions}>
         <LoadingContainer>
-          <MyComponent />
+          <MyContainer />
         </LoadingContainer>
       </DrizzleProvider>
     );
--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,20 +1,19 @@
+import Web3 from "web3";
 import SimpleStorage from "./contracts/SimpleStorage.json";
+import ComplexStorage from "./contracts/ComplexStorage.json";
+import TutorialToken from "./contracts/TutorialToken.json";
 
 const options = {
   web3: {
     block: false,
-    fallback: {
-      type: "ws",
-      url: "ws://127.0.0.1:9545",
-    },
+    customProvider: new Web3("ws://localhost:8545"),
   },
-  contracts: [SimpleStorage],
+  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
   events: {
     SimpleStorage: ["StorageSet"],
   },
   polls: {
-    // set polling interval to 30secs so we don't get buried in poll events
-    accounts: 30000,
+    accounts: 1500,
   },
 };
 
